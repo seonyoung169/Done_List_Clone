4121fa62c5b28649eb2d47eb12e82368e8733679
sunnyangee <hwagae2003@gmail.com>
2021-08-09 20:21:14 +0900

    [develop] 프로젝트 생성

---
+++ DoneList_Clone/DoneList_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoneList_Clone
+//
+//  Created by SEONYOUNG LEE on 2021/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
