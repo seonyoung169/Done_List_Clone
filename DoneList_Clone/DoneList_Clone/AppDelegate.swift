28884048f2c40d164c4b95bf2e9fab3916ef1de0
sunnyangee <hwagae2003@gmail.com>
2021-11-24 17:17:26 +0900

    [report] AppDelegate navigation controller 코드 적용

--- DoneList_Clone/DoneList_Clone/AppDelegate.swift
+++ DoneList_Clone/DoneList_Clone/AppDelegate.swift
@@ -10,10 +10,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let homeController = storyboard.instantiateViewController(withIdentifier: "HomeVC") as! HomeVC
+        let navigationController = UINavigationController(rootViewController: homeController)
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
